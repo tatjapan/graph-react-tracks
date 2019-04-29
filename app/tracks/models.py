7d157c16d4127f58494b0fcf238ce19cf64af2e8
Tatsuya Tsuri <tatsuya.tsuri@joltieee.com>
2019-04-29 11:27:36 +0900

    First commit

---
+++ app/tracks/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Track(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
