ac5aefdc23eddbd79318806bf34003872aa600b2
Tatsuya Tsuri <tatsuya.tsuri@joltieee.com>
2019-04-30 16:27:11 +0900

    Fix root message in indexjs

--- graph-react-tracks-client/src/index.js
+++ graph-react-tracks-client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import Root from "./Root";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(<Root />, document.getElementById("root!"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
