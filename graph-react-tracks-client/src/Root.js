dee2a781079386890686466e4af5ff6e6795c7db
Tatsuya Tsuri <tatsuya.tsuri@joltieee.com>
2019-05-01 19:51:45 +0900

    Add logout function

--- graph-react-tracks-client/src/Root.js
+++ graph-react-tracks-client/src/Root.js
@@ -1,30 +1,58 @@
 import React from "react";
-import withRoot from "./withRoot";
-
 import { Query } from 'react-apollo';
 import { gql } from 'apollo-boost';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import withRoot from "./withRoot";
+import App from './pages/App';
+import Profile from './pages/Profile';
+import Header from './components/Shared/Header';
+import Loading from './components/Shared/Loading';
+import Error from './components/Shared/Error';
 
 const Root = () => (
-    <Query query={GET_TRACKS_QUERY}>
+    <Query query={ME_QUERY}>
         {({ data, loading, error }) => {
-            if (loading) return <div>Loading</div>
-            if (error) return <div>Error</div>
+            if (loading) return <Loading />
+            if (error) return <Error error={error} />
 
-            return <div>{JSON.stringify(data)}</div>
+            const currentUser = data.me
+
+            return (
+                <Router>
+                    <>
+                        <Header currentUser={currentUser}/>
+                        <Switch>
+                            <Route exact path="/" component={App} />
+                            <Route path="/profile/:id" component={Profile} />
+                        </Switch>
+                    </>
+                </Router>
+            )
         }}
     </Query>
-)
+);
 
-const GET_TRACKS_QUERY = gql`
-    {
-        tracks {
-            id
-            title
-            description
-            hashtag
-            url
+const ME_QUERY = gql`
+        {
+            me {
+                id
+                username
+                email
+            }
         }
-    }
-`
+`;
+
+
+// const GET_TRACKS_QUERY = gql`
+//     {
+//         tracks {
+//             id
+//             title
+//             description
+//             hashtag
+//             url
+//         }
+//     }
+// `
 
 export default withRoot(Root);
--- graph-react-tracks-client/src/components/Shared/Header.js
+++ graph-react-tracks-client/src/components/Shared/Header.js
@@ -1,13 +1,41 @@
 import React from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import RadioIcon from "@material-ui/icons/RadioTwoTone";
-// import FaceIcon from "@material-ui/icons/FaceTwoTone";
-// import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import RadioIcon from "@material-ui/icons/RadioTwoTone";
+import FaceIcon from "@material-ui/icons/FaceTwoTone";
+import Typography from "@material-ui/core/Typography";
+import { Link } from 'react-router-dom';
 
-const Header = ({ classes }) => {
-  return <div>Header</div>;
+import Signout from '../Auth/Signout';
+
+const Header = ({ classes, currentUser }) => {
+  return (
+    <AppBar position="static" className={classes.root}>
+      <Toolbar>
+        {/* Title and logo */}
+        <Link to="/" className={classes.grow}>
+          <RadioIcon className={classes.logo} color='secondary' />
+          <Typography variant="headline" color="secondary" noWrap>
+            Trackgram
+          </Typography>
+        </Link>
+
+        {/* Auth user info */}
+        {currentUser && (
+          <Link to={`/profile/${currentUser.id}`} className={classes.grow}>
+            <FaceIcon className={classes.faceIcon} />
+            <Typography variant="headline" className={classes.username} noWrap>
+              {currentUser.username}
+            </Typography>
+          </Link>
+        )}
+
+        {/* Signout */}
+        <Signout />
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 const styles = theme => ({
